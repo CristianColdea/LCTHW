81c56b5f1e1bd821a54527342e8a96c78893da84
CristianColdea <rdt333@gmail.com>
2022-08-23 15:09:25 +0300

    2D array Python

---
+++ ex13_2darray.py
@@ -0,0 +1,9 @@
+# example of a 2D array (i.e. array of strings) implemented in Python
+
+states = ["California", "Oregon",
+          "Washington", "Texas"]
+
+for state in states:
+    print()
+    for letter in state:
+         print(letter)
